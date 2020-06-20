8e25c8ba2463ae60e8e6863d1b8165161af39e15
Anugrah Shukla <anugrah.shukla@docsapp.in>
2020-06-20 21:52:37 +0530

    public dummy folder

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var controllers = require('./controllers');
+var socket = require('socket.io');
 
 app.use(bodyParser.json());
 app.get('/',controllers.showHomePage);
 app.post('/signup',controllers.signup);
 app.post('/login',controllers.login);
-app.post('/sendMessage',controllers.sessionValidator,controllers.sendMessage);
+app.post('/sendMessage',controllers.sendMessage);
 
 app.listen('8081');
 
